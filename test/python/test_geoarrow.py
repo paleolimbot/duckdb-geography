94a3ee19eab8ae2d5250d97ea224bbf5a5f44cfe
Max Gabrielsson <max@gabrielsson.com>
2025-08-19 11:24:46 +0200

    format tests

--- test/python/test_geoarrow.py
+++ test/python/test_geoarrow.py
@@ -16,9 +16,7 @@ def test_export_without_register(con):
 
 
 def test_basic_export(geoarrow_con):
-    tab = geoarrow_con.sql(
-        """SELECT s2_geogfromtext('POINT (0 1)') as geom;"""
-    ).to_arrow_table()
+    tab = geoarrow_con.sql("""SELECT s2_geogfromtext('POINT (0 1)') as geom;""").to_arrow_table()
 
     pa_type = tab["geom"].type
     assert isinstance(pa_type, ga.GeometryExtensionType)
@@ -38,18 +36,14 @@ def test_basic_import(geoarrow_con):
             "ARROW:extension:metadata": '{"edges": "spherical"}',
         },
     )
-    point_wkb = (
-        b"\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00$@\x00\x00\x00\x00\x00\x004@"
-    )
+    point_wkb = b"\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00$@\x00\x00\x00\x00\x00\x004@"
     schema = pa.schema([field])
     geo_table = pa.table(
         [pa.array([point_wkb])],
         schema=schema,
     )
 
-    tab = geoarrow_con.sql(
-        """SELECT s2_astext(geometry) as wkt FROM geo_table;"""
-    ).to_arrow_table()
+    tab = geoarrow_con.sql("""SELECT s2_astext(geometry) as wkt FROM geo_table;""").to_arrow_table()
     assert tab["wkt"].to_pylist() == ["POINT (10 20)"]
 
 
@@ -70,12 +64,8 @@ def test_reject_planar_edges(geoarrow_con):
 
 def test_roundtrip_countries(geoarrow_con):
     countries_file = HERE.parent.parent / "data" / "countries.tsv"
-    geo_table = geoarrow_con.sql(
-        f"""SELECT s2_geogfromtext(geog) as geog1 FROM '{countries_file}'"""
-    ).to_arrow_table()
-    geo_table2 = geoarrow_con.sql(
-        """SELECT geog1 as geog2 FROM geo_table"""
-    ).to_arrow_table()
+    geo_table = geoarrow_con.sql(f"""SELECT s2_geogfromtext(geog) as geog1 FROM '{countries_file}'""").to_arrow_table()
+    geo_table2 = geoarrow_con.sql("""SELECT geog1 as geog2 FROM geo_table""").to_arrow_table()
 
     table_both = pa.table(
         [geo_table["geog1"], geo_table2["geog2"]],
@@ -91,11 +81,6 @@ def test_spherely_interop(geoarrow_con):
     import spherely
     import numpy as np
 
-    geo_table = geoarrow_con.sql(
-        """SELECT geog, s2_area(geog) as area FROM s2_data_countries()"""
-    ).to_arrow_table()
+    geo_table = geoarrow_con.sql("""SELECT geog, s2_area(geog) as area FROM s2_data_countries()""").to_arrow_table()
     geogs = spherely.from_geoarrow(geo_table["geog"].chunk(0))
-    np.testing.assert_array_almost_equal(
-        spherely.area(geogs), geo_table["area"].to_numpy(),
-        decimal=1
-    )
+    np.testing.assert_array_almost_equal(spherely.area(geogs), geo_table["area"].to_numpy(), decimal=1)
